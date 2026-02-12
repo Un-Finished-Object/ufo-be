e92547c171c85daa2a1025ba777f77ddac0e7735
HSHwan <tnghks2866@gmail.com>
2026-02-12 14:09:16 +0900

    init: 프로젝트 초기 설정 구축

---
+++ src/main/java/com/ufo/ufo/UfoBeApplication.java
@@ -0,0 +1,13 @@
+package com.ufo.ufo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoBeApplication.class, args);
+	}
+
+}
