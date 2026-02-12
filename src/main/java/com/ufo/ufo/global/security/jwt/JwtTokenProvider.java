d5ebb67df008e2f12d67618e1dea0e46804dae86
HSHwan <tnghks2866@gmail.com>
2026-02-13 03:08:15 +0900

    feat: JWT 토큰 생성 및 검증 구현

---
+++ src/main/java/com/ufo/ufo/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,108 @@
+package com.ufo.ufo.global.security.jwt;
+
+import com.ufo.ufo.global.security.types.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey key;
+
+    @Getter
+    @Value("${spring.jwt.access-token-expire}")
+    private long accessTokenExpireTime;
+
+    @Getter
+    @Value("${spring.jwt.refresh-token-expire}")
+    private long refreshTokenExpireTime;
+
+    public JwtTokenProvider(@Value("${spring.jwt.secret}") String secret) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createAccessToken(String email, String role) {
+        return createToken(email, role, accessTokenExpireTime);
+    }
+
+    public String createRefreshToken(String email) {
+        return createToken(email, null, refreshTokenExpireTime);
+    }
+
+    private String createToken(String email, String role, long expireTime) {
+        final Date now = new Date();
+        final Date expiredDate = new Date(now.getTime() + expireTime);
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(now)
+                .expiration(expiredDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+        String role = claims.get("role", String.class);
+
+        String roleName = (role != null) ? role : Role.ROLE_GUEST.name();
+
+        UserDetails principal = new User(claims.getSubject(), "",
+                Collections.singleton(new SimpleGrantedAuthority(roleName)));
+
+        return new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
+
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
