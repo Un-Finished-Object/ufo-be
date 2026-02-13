1e310d0387facefaf9209fd905b0de99388a258d
HSHwan <tnghks2866@gmail.com>
2026-02-13 12:19:00 +0900

    fix: refreshToken 쿠키 추출

--- src/main/java/com/ufo/ufo/domain/auth/api/AuthController.java
+++ src/main/java/com/ufo/ufo/domain/auth/api/AuthController.java
@@ -5,7 +5,7 @@ import com.ufo.ufo.domain.auth.dto.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/token/refresh")
-    public ResponseEntity<TokenResponse> refreshToken(@RequestHeader("Cookie") String refreshToken) {
-        String token = refreshToken.replace("Bearer ", "");
-        return ResponseEntity.ok(authService.reissue(token));
+    public ResponseEntity<TokenResponse> refreshToken(@CookieValue("refreshToken") String refreshToken) {
+        return ResponseEntity.ok(authService.reissue(refreshToken));
     }
 }
