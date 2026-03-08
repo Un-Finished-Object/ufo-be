1a6802fe1d6fa759a117b081cf0da1590803c354
HSHwan <tnghks2866@gmail.com>
2026-03-08 23:38:17 +0900

    feat: WebSocket 연결 시 사용자 식별 기능 구현

--- src/main/java/com/ufo/ufo/global/config/WebSocketConfig.java
+++ src/main/java/com/ufo/ufo/global/config/WebSocketConfig.java
@@ -1,19 +1,30 @@
 package com.ufo.ufo.global.config;
 
+import com.ufo.ufo.global.security.websocket.StompJwtAuthChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompJwtAuthChannelInterceptor stompJwtAuthChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompJwtAuthChannelInterceptor);
     }
 
     @Override
