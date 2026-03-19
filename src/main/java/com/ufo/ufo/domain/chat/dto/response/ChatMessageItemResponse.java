e897d98350686f3830b942702061f282f216cb5a
HSHwan <tnghks2866@gmail.com>
2026-03-19 13:38:27 +0900

    feat: 채팅 메시지 조회 응답에 송신자 닉네임 추가

--- src/main/java/com/ufo/ufo/domain/chat/dto/response/ChatMessageItemResponse.java
+++ src/main/java/com/ufo/ufo/domain/chat/dto/response/ChatMessageItemResponse.java
@@ -4,12 +4,14 @@ import com.ufo.ufo.domain.chat.domain.ChatMessage;
 import java.time.LocalDateTime;
 
 public record ChatMessageItemResponse(
+        String userName,
         Long messageId,
         String text,
         LocalDateTime createdAt
 ) {
     public static ChatMessageItemResponse from(ChatMessage chatMessage) {
         return new ChatMessageItemResponse(
+                chatMessage.getUser().getNickname(),
                 chatMessage.getId(),
                 chatMessage.getText(),
                 chatMessage.getCreatedAt()
